53d4de8178c57e9af83676a6bc9750a9dc54b7b4
guancongyi <congyiguan@gmail.com>
2020-02-22 14:16:40 -0800

    finished switching function

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 const Koa = require("koa")
 const Router = require("koa-router");
+const views = require("koa-views");
 const static = require("koa-static");
-// const koaBody = require("koa-body");
-
+const koaBody = require("koa-body");
 
 let app = new Koa();
-app.use(static(__dirname + "/static"));
-// app.use(koaBody());
 let router = new Router();
 
-
-router.get("/",ctx=>{
-    ctx.render("index.pug");
-})
-
-router.get("/world", ctx=>{
-    ctx.body = 'world'
-})
-
-router.get("/world_restaurant", ctx=>{
-    ctx.body = 'world_restua'
-})
-
-router.get("/us_restaurant", ctx=>{
-    ctx.body = 'us'
-})
-
+app.use(views(__dirname+"/views"),{
+    extension:"pug"
+});
 app.use(router.routes());
+app.use(static(__dirname + "/static"));
+app.use(koaBody());
+
 app.listen(8787);
+
+
+router.get("/", ctx=>{
+    ctx.redirect("/world");
+});
+
+router.get("/index",ctx=>{
+    ctx.redirect("/world");
+});
+
+router.post("/switch",  ctx=>{
+    // console.log(ctx.request.body['typeOfDb'])
+    // if (ctx.request.body['typeOfDb'] == 'world'){
+        ctx.redirect("/world");
+    // }
+});
+
+router.get("/world",async ctx=>{
+    await ctx.render("world.pug");
+});
+
+router.get("/world_restaurant", async ctx=>{
+    await ctx.render("world_restaurant.pug");
+})
+
+router.get("/us_restaurant", async ctx=>{
+    await ctx.render("us_restaurant.pug");
+    
+})
+
