feed1dafbd253b9c0f5ee7d0081dcb4ad444fe64
guancongyi <congyiguan@gmail.com>
2020-03-20 23:14:10 -0700

    fixed table format

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import DB from './process.js'
 let database = new DB();
 let currDB = 'world';
 
+
+let widthOfHeaderCells = []
 function submit(){
     if (currDB == 'world'){
         let keyword = $('#keyword').val();
@@ -14,12 +16,16 @@ function submit(){
                 console.log(arr[i])
                 let id = "#t"+(i+1);
                 console.log(id)
-                renderTable(id, Object.keys(arr[i][0]), 1);
+                fillTable(id, Object.keys(arr[i][0]), 1);
                 for (let j = 0; j < arr[i].length; j++){
                     // console.log(Object.values(arr[i][j]))
-                    renderTable(id, Object.values(arr[i][j]), 0)
+                    fillTable(id, Object.values(arr[i][j]), 0)
                 }
             }
+            renderTable("#t1")
+            renderTable("#t2")
+            renderTable("#t3")
+
         });
     }
 
@@ -28,26 +34,29 @@ function submit(){
 
 
 (function init(){
+
     // when submit button is clicked
     $('#submit').click(function(){submit()});
-
-    // $('#t1').on('scroll', function(){
-    //     $('thead').addClass("position","fixed")
-    //     console.log(1)
-    // })
-
+    
     // input change event listener
     $('#select_db input').on('change', function() {
         // console.log($("#t1").children()[0])
+
+        // alignWithHeader($('#t1').children('#header'), $('#t1').children('#header'))
         currDB = $('input[name=db]:checked', '#select_db').val();
         if(currDB == 'world'){
             appendWorldFilters();
             // get default data
             database.getData("country",null).then((data)=>{
-                renderTable("#t1", Object.keys(data[0]), 1);
+                fillTable("#t1", Object.keys(data[0]), 1);
+
                 for (let i=0; i<data.length;i++){
-                    renderTable("#t1", Object.values(data[i]), 0);
+                    fillTable("#t1", Object.values(data[i]), 0);
                 }
+                renderTable(id);
+                
+
+                
             })
 
         }else if (currDB == 'world_restaurant'){
@@ -57,35 +66,48 @@ function submit(){
             appendUsRestaurantFilters();
             // prepTable('<tr><th>ID</th><th>City</th><th>Name</th><th>Stars</th><th>Open Now</th></tr>');
         }
+
+
         
     });
 })();
 
-function renderTable(id, row, isHeader){
+function renderTable(id){
+    $(id).DataTable({
+        "scrollY":        "250px",
+        "scrollX":        "90%",
+        "paging":         false,
+        "info":false
+    });
+}
+function fillTable(id, row, isHeader){
     let res = "<tr>"
     
     if (!isHeader){
-        // $(id).children("#body").empty();
-        // $(id).append('<tbody id="body"></tbody>')
         for (let i = 0; i<row.length;i++ ){
             res += "<td>"+row[i]+"</td>";
         }
         res+="</tr>";
-        // console.log($(id).children("#scroll"))
-        $(id).children("#body").append(res)
+        $(id).children("#tb_body").append(res)
         // console.log($(id).children("#body"))
+        
     }else{
-        $(id).children("#header").empty();
 
+        $(id).children("#tb_header").empty();
+        
         for (let i = 0; i<row.length;i++ ){
             res += "<th>"+row[i]+"</th>";
         }
         res+="</tr>";
-        $(id).children("#header").append(res);
+        $(id).children("#tb_header").append(res);
 
     }
     
+
+    
 }
+
+  
 
 function appendWorldFilters(){
     $('#filter_options').empty();
